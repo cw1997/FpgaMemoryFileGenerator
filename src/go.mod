ebf3978c5a40b8f3a903fb41a5cf1bcb948a1804
昌维 <867597730@qq.com>
2019-12-08 01:37:20 +0800

    Add and init go mod and require urfave/cli/v2 and add test code

--- src/go.mod
+++ src/go.mod
@@ -1,3 +1,5 @@
 module FpgaMemoryFileGenerator
 
 go 1.13
+
+require github.com/urfave/cli/v2 v2.0.0 // indirect
--- src/main.go
+++ src/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "log"
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/urfave/cli/v2"
+)
+
+const (
+	NAME = "FpgaMemoryFileGenerator"
+	MAJOR_VERSION_NUMBER = 0
+	MINOR_VERSION_NUMBER = 1
+	REVISION_VERSION_NUMBER = 0
+)
+
+var (
+	//timeStr = time.Now().Format("2006-01-02 15:04:05")
+	BUILD_TIME = time.Now().Format("2006-01-02 15:04:05")
+)
 
 func main() {
 	log.Println("ok")
+
+	version := fmt.Sprintf("%d.%d.%d Build %s", MAJOR_VERSION_NUMBER, MINOR_VERSION_NUMBER, REVISION_VERSION_NUMBER, BUILD_TIME)
+
+	cli.VersionPrinter = func(c *cli.Context) {
+		fmt.Printf("all2mif version all2mif%s", c.App.Version)
+	}
+
+	app := &cli.App{
+		Name: NAME,
+		Version: version,
+	}
+	app.Run(os.Args)
+
+
 }
