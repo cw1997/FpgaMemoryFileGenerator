c44995218b674b9717381e0c4d7293d1a0746433
昌维 <867597730@qq.com>
2019-12-08 21:17:51 +0800

    Add file helper

---
+++ src/io/file.go
@@ -0,0 +1,18 @@
+// project : FpgaMemoryFileGenerator
+// author : cw1997 [www.changwei.me]
+// datetime : 2019/12/8 21:07
+// repo : https://github.com/cw1997/FpgaMemoryFileGenerator
+
+package io
+
+import "io/ioutil"
+
+func WriteStrToFile(data string, path string) error {
+	err := ioutil.WriteFile(path, []byte(data), 0777)
+	return err
+}
+
+func ReadBytesFromFile(data string, path string) ([]byte, error) {
+	b, err := ioutil.ReadFile(path)
+	return b, err
+}
