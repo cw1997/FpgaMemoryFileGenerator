49103558f2e9df2b5a827a222e29c7208aa86939
昌维 <867597730@qq.com>
2019-12-08 22:31:00 +0800

    go fmt generator/coe

--- src/generator/coe/coe_test.go
+++ src/generator/coe/coe_test.go
@@ -8,13 +8,13 @@ package coe
 import "testing"
 
 type params struct {
-	width int
+	width     int
 	dataRadix int
-	data []byte
+	data      []byte
 }
 
 type paramsNew struct {
-	width int
+	width     int
 	dataRadix int
 }
 
@@ -22,16 +22,15 @@ func TestNewMifGenerator(t *testing.T) {
 	var testCases = []struct {
 		in       paramsNew // input
 		expected Format    // expected result
-	} {
+	}{
 		//{{depth: 8, width: 8, addressRadix: "hex", dataRadix: "hex", data: data1}, "16"},
 		{paramsNew{8, 8},
 			Format{8, 8}},
 	}
 
-
 	for _, tt := range testCases {
-		actual := NewCoeGenerator( tt.in.width, tt.in.dataRadix)
-		if actual.width != tt.expected.width || actual.dataRadix != tt.expected.dataRadix  {
+		actual := NewCoeGenerator(tt.in.width, tt.in.dataRadix)
+		if actual.width != tt.expected.width || actual.dataRadix != tt.expected.dataRadix {
 			t.Errorf("Generate(%v) = %v; expected %v", tt.in, actual, tt.expected)
 		}
 	}
@@ -50,19 +49,17 @@ memory_initialization_vector=
 	var testCases = []struct {
 		in       params // input
 		expected string // expected result
-	} {
+	}{
 		//{{depth: 8, width: 8, addressRadix: "hex", dataRadix: "hex", data: data1}, "16"},
 		{params{8, 8, data1}, except1},
 	}
-
 
 	for _, tt := range testCases {
 		f := NewCoeGenerator(tt.in.width, tt.in.dataRadix)
 		actual := f.Generate(tt.in.data)
 		if actual != tt.expected {
-			t.Errorf("Generate(%v) \n ========== actual start ========== \n%v\n  ========== actual end ========== \n" +
+			t.Errorf("Generate(%v) \n ========== actual start ========== \n%v\n  ========== actual end ========== \n"+
 				"expected : \n ========== expected start ========== \n%v\n  ========== expected end ========== \n", tt.in, actual, tt.expected)
 		}
 	}
 }
-
